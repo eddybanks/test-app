aee588b74ab47e3084d894993c85fa83b0eec3d7
Edwin Agbenyega <eddybanks03@gmail.com>
2022-03-20 19:30:03 -0700

    Basic swr functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import useSWR from "swr";
+
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 function App() {
+  const { data, error } = useSWR(
+    "https://gateway.marvel.com/v1/public/characters?apikey=c02cfe0275994129e91bb12daceca74e",
+    fetcher
+  );
+
+  if (!data) return <p>Loading!</p>;
+  if (error) throw error;
+
+  const marvelData = data.data.results;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="marvel-cards">
+        {marvelData.map((mavItem) => (
+          <div className="marvel-card" key={mavItem.id}>
+            <img
+              src={mavItem.thumbnail.path + "." + mavItem.thumbnail.extension}
+              alt={mavItem.name}
+            />
+            <h1>{mavItem.name}</h1>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
